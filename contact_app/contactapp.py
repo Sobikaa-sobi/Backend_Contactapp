c3041fc3c5461dcc63b373e62abefc7acb79bc91
Sobikaa-sobi <66825553+Sobikaa-sobi@users.noreply.github.com>
2020-07-10 10:57:10 +0530

    contactapp_DB_design

--- contact_app/contactapp.py
+++ contact_app/contactapp.py
@@ -1,11 +1,11 @@
 import mysql.connector
 db=mysql.connector.connect(host="localhost",user="root",passwd="sobikaa3723",database="contact")
 cur=db.cursor()
-#cur.execute("create table Contacts(contact_name varchar(20),phone_no int NOT NULL,email varchar(30) NOT NULL,image varchar(15),address varchar(30),CONSTRAINT UC_Contacts UNIQUE (phone_no,email))")
-#cur.execute("ALTER TABLE Contacts MODIFY phone_no VARCHAR(10)")
-#ins="insert into Contacts(contact_name,phone_no,email,image,address) values(%s,%s,%s,%s,%s)"
-#contact_details=[("sam","8976543210","sam@gmail.com","img1.jpg","chennai"),("jach","8978743210","jack@gmail.com","img2.jpg","coimbatore"),("micky","9765432180","micky@gmail.com","img3.jpg","chennai")]
-#cur.executemany(ins,contact_details)
+cur.execute("create table Contacts(contact_name varchar(20),phone_no int NOT NULL,email varchar(30) NOT NULL,image varchar(15),address varchar(30),CONSTRAINT UC_Contacts UNIQUE (phone_no,email))")
+cur.execute("ALTER TABLE Contacts MODIFY phone_no VARCHAR(10)")
+ins="insert into Contacts(contact_name,phone_no,email,image,address) values(%s,%s,%s,%s,%s)"
+contact_details=[("sam","8976543210","sam@gmail.com","img1.jpg","chennai"),("jach","8978743210","jack@gmail.com","img2.jpg","coimbatore"),("micky","9765432180","micky@gmail.com","img3.jpg","chennai")]
+cur.executemany(ins,contact_details)
 print("ALL DATA BEFORE OPERATIONS")
 cur.execute("select * from Contacts")
 data=cur.fetchall()
